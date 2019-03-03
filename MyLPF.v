d9a97fa1f1b5c36aa4e2bb1f1b52507da7d2fadb
rapidnack <rapidnack@gmail.com>
2019-03-03 22:36:06 +0900

    change FIR

--- MyLPF.v
+++ MyLPF.v
@@ -282,6 +282,14 @@ module MyLPF
 				end
 				else begin
 					deci_cnt <= deci_cnt + 1;
+					
+					if (cnt < NUM_TAPS) begin
+						cnt <= cnt + 1;
+					end
+					if (cnt < NUM_TAPS) begin
+						y <= y + h[cnt] * x;
+					end
+					ast_source_valid <= 1'b0;
 				end
 			end
 			else begin
